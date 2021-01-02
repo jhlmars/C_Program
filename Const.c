e4d9221bf933356a5563402ab146ff4adcbd86ea
jhlmars <jhlmars@gmail.com>
2021-01-02 18:41:29 +0900

    feature/constant 브랜치 생성, Const.c 추가

---
+++ Const.c
@@ -0,0 +1,17 @@
+// 부동소수점형 정밀도 차이
+#include <stdio.h>
+
+int main(void)
+{
+    float fvalue = 1234567890.12345678901234567890;
+    double dvalue = 1234567890.12345678901234567890;
+
+    printf("Float : %f\n",fvalue);
+    printf("Double : %lf\n",dvalue);
+
+    return 0;
+}
+
+
+
+
