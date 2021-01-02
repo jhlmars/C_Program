11ebfd4bf21711de8598093e5ab28fee8966bd86
jhlmars <jhlmars@gmail.com>
2021-01-02 15:19:35 +0900

    첫번쨰 커밋 올리기 Helo.c

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		printf("Hello World\n");
+		printf("C program\n");
+		printf("Make Conflict\n");
+		printf("Compile Process\n");
+	}
+	return 0;
+}
